4fe4e9d8ba947e5e4cc1e64ee87dcbcec5057c31
adane <102168697+habeshaethiopia@users.noreply.github.com>
2023-12-05 21:28:26 +0300

    Added solution for Non-decreasing Array

--- leet-code-solutions/non-decreasing-array.py
+++ leet-code-solutions/non-decreasing-array.py
@@ -2,12 +2,15 @@ class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
         
         count = 0
-        min = nums[-1]
+        # check for the odd situation
         for i in range(len(nums)-1):
+            # check for any situation(increasing situation)
             if nums[i]>nums[i+1]:
                 count +=1
+                # handling the test cases like [3,4,2,3] must be false
                 if i>0 and nums[i-1]> nums[i+1]:
                     nums[i+1]=nums[i]
+
         if count > 1:
             return False
         else:
