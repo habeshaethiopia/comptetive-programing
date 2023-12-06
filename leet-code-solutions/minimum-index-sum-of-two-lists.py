f5e5d91184c571db0d542012d1b864fe60d3b305
adane <102168697+habeshaethiopia@users.noreply.github.com>
2023-12-06 17:51:55 +0300

    Added solution for Minimum Index Sum of Two Lists

--- leet-code-solutions/minimum-index-sum-of-two-lists.py
+++ leet-code-solutions/minimum-index-sum-of-two-lists.py
@@ -2,13 +2,13 @@ class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
         min = float("inf")
         result=[]
-        for i in list1:
-            if i in list2:
-                index = list1.index(i) + list2.index(i)
-                if min> index:
-                    min = index
-                    result = [i]
-                elif min == index:
-                    result.append(i)
+        for item in list1:
+            if item in list2:
+                index_sum = list1.index(item) + list2.index(item)
+                if min > index_sum:
+                    min = index_sum
+                    result = [item]
+                elif min == index_sum:
+                    result.append(item)
         return result
         
