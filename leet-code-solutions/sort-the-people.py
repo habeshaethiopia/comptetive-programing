71f792f55af5278d44bc30d91fdd4356db1f07bf
adane <102168697+habeshaethiopia@users.noreply.github.com>
2023-12-26 18:07:15 +0300

    Added solution for Sort the People

--- leet-code-solutions/sort-the-people.py
+++ leet-code-solutions/sort-the-people.py
@@ -2,9 +2,9 @@ class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         
         name=dict(zip(heights, names))
-        heights.sort(reverse=True)
-        # for i in range(len(heights)):
-        #     for j in range(i+1, len(heights)):
-        #         if heights[i]<heights[j]:
-        #             heights[i+1],heights[i]=heights[i],heights[i+1]
+        # heights.sort(reverse=True)
+        for i in range(len(heights)):
+            for j in range(len(heights)-i-1):
+                if heights[j+1]>heights[j]:
+                    heights[j+1],heights[j]=heights[j],heights[j+1]
         return [name[i] for i in heights]
