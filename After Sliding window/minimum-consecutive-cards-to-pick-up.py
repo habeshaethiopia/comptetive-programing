41e3f4e7f8061d58b751f7cae9270e84d4f0fcef
adane <102168697+habeshaethiopia@users.noreply.github.com>
2024-01-18 14:08:07 +0300

    Added solution for Minimum Consecutive Cards to Pick Up

--- After Sliding window/minimum-consecutive-cards-to-pick-up.py
+++ After Sliding window/minimum-consecutive-cards-to-pick-up.py
@@ -1,14 +1,14 @@
 class Solution:
     def minimumCardPickup(self, cards: List[int]) -> int:
         l=0
-        c=0
-        w={}
+        w=set()
         ans=float('inf')
         for i in range(len(cards)):
-            if not cards[i] in w:
-                w[cards[i]]=i
-            else:
-                ans=min(i-w[cards[i]]+1, ans)
-                w[cards[i]]=i
+            while cards[i] in w:
+                ans=min(i-l+1, ans)
+                # print(cards[l:i+1])
+                w.discard(cards[l])
+                l+=1
+            w.add(cards[i])
         return -1 if ans==float('inf') else ans
 
