c46f4ac0bb4f683f7e30ee39c843f7a49de1a1d2
adane <102168697+habeshaethiopia@users.noreply.github.com>
2024-03-06 10:19:07 +0300

    Add solution for Sqrt(x)

---
+++ leetcode/sqrtx.py
@@ -0,0 +1,8 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        for i in range(x//2 +2):
+            if i*i==x:
+                return i
+            if i*i>x:
+                return i-1
+        
