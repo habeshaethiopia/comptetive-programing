06cdc8e45cfc9b246346412ad8c31260978accef
adane <102168697+habeshaethiopia@users.noreply.github.com>
2024-05-14 22:28:10 +0300

    Created 'Unique Paths' by A2SV Hub

---
+++ 14-May-2024/Unique Paths 150283.py
@@ -0,0 +1,24 @@
+# Problem: Unique Paths - https://leetcode.com/problems/unique-paths/
+
+class Solution:
+    def uniquePaths(self, n: int, m: int) -> int:
+        path = [(1, 0), (0, 1)]
+
+        def check(x, y):
+            return x < n and y < m
+
+        @cache
+        def dp(x, y):
+            # print(x,y)
+            if x == n - 1 and y == m - 1:
+                return 1
+            ans = 0
+            for i, j in [[1, 0], [0, 1]]:
+                r, c = x + i, y + j
+                
+                if r < n and c < m:
+                    ans += dp(r, c)
+            # print('ans',ans)
+            return ans
+
+        return dp(0, 0)
