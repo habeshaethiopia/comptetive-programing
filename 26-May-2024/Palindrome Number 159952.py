54e80cc4019410e9116623a4dbcb12c3d9878ce5
adane <102168697+habeshaethiopia@users.noreply.github.com>
2024-05-26 23:50:28 +0300

    Created 'Palindrome Number' by A2SV Hub

---
+++ 26-May-2024/Palindrome Number 159952.py
@@ -0,0 +1,8 @@
+# Problem: Palindrome Number - https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x=str(x)
+        i = len(x)
+
+        return x==x[::-1]
