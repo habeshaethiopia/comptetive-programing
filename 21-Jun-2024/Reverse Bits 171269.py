63563f097974b7b77fde7a1c81f65d6a3ba52c61
adane <adman19940805@gmail.com>
2024-06-21 22:42:19 +0300

    Created 'Reverse Bits' by A2SV Hub

---
+++ 21-Jun-2024/Reverse Bits 171269.py
@@ -0,0 +1,7 @@
+# Problem: Reverse Bits - https://leetcode.com/problems/reverse-bits/
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        bit=bin(n)[2:]
+        bit='0'*(32-len(bit))+bit
+        return int(bit[::-1],2)
