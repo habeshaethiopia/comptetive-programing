175da259b1312bb46cf3e21f37dc8eef41d67f65
adane <adman19940805@gmail.com>
2024-06-25 20:18:43 +0300

    Created 'Number of Wonderful Substrings' by A2SV Hub

---
+++ 25-Jun-2024/Number of Wonderful Substrings 172534.py
@@ -0,0 +1,20 @@
+# Problem: Number of Wonderful Substrings - https://leetcode.com/problems/number-of-wonderful-substrings/
+
+class Solution:
+    def wonderfulSubstrings(self, word: str) -> int:
+        cnt = defaultdict(int)
+        cnt[0]=1
+        pre=0
+        ans=0
+        for c in word:
+            pre=pre^(1<<(ord(c)-ord('a')))
+            ans+=cnt[pre] #even pattern
+            for i in range(ord('a'),ord('j')+1):
+                odd=pre^(1<<(i-ord('a')))
+                ans+=cnt[odd]
+            cnt[pre]+=1
+
+
+        return ans
+
+            
