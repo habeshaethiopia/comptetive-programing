8a95a8fda3b97a1794c97f0c43d93cfe9553be21
adane <adman19940805@gmail.com>
2024-06-26 22:50:31 +0300

    Created 'Watering Plants' by A2SV Hub

---
+++ 26-Jun-2024/Watering Plants 173073.py
@@ -0,0 +1,16 @@
+# Problem: Watering Plants - https://leetcode.com/problems/watering-plants/
+
+class Solution:
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+        temp=capacity
+        sm=0
+        for i in range(len(plants)):
+            if temp >= plants[i]:
+                sm+=1
+                temp-=plants[i]
+              
+            else:
+                sm+=i+i+1
+                temp=capacity -plants[i]
+            print(temp)
+        return sm
