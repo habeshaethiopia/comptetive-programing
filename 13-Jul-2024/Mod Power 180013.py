190163563cfd4056665f17270b2f1523edb35483
adane <adman19940805@gmail.com>
2024-07-13 22:36:09 +0300

    Created 'Mod Power' by A2SV Hub

---
+++ 13-Jul-2024/Mod Power 180013.py
@@ -0,0 +1,9 @@
+# Problem: Mod Power - https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+
+from math import pow
+a=int(input())
+b=int(input())
+c=int(input())
+valu=int(a**b)
+print(valu)
+print(valu%c)
